967a2771299a04617c0ad6425e398685da4146c2
leuthier <leuthier@users.noreply.github.com>
2024-03-14 00:28:26 -0300

    Added and updated aliases for improved command line efficiency and Android development tasks. Also included functions for Android device management and SSH tunneling.
    Changes:
    - Feat: Updated Android SDK
    - Feat: Added functions for managing Android devices connected via ADB. Included SSH tunneling alias for device access.
    - Feat: Aliases for various Android device operations like rebooting, capturing screenshots, and recording screen
    - Feat: Aliases for directory navigation and bashrc editing
    - Feat: Aliases for killing ADB and Vysor processes.
    
    Notes:
    Please ensure proper configuration and permissions for executing these commands.
    Vysor application (https://www.vysor.io/download/) is required for some functionalities.

---
+++ windows/update_android_sdk.bat
@@ -0,0 +1,33 @@
+﻿:: Download latest adb & fastboot (android-sdk) from Google servers and update automatically using command prompt only
+:: Created by @leuthier - March 2024
+
+
+@ECHO OFF
+SETLOCAL
+
+
+echo "Checking adb & fastboot versions"
+adb --version
+fastboot --version
+
+
+echo "Killing adb service"
+adb kill-server
+
+
+echo "Downloading adb service"
+bitsadmin /transfer /download /priority FOREGROUND "https://dl.google.com/android/repository/platform-tools-latest-windows.zip" "C:\android-sdk-windows-terminal\platform-tools-latest-windows.zip"
+cd "C:\android-sdk-windows-terminal"
+ren "platform-tools" "platform-tools_old"
+tar -xf platform-tools-latest-windows.zip
+ren "platform-tools-latest-windows" "platform-tools"
+
+
+adb --version
+fastboot --version
+
+
+echo "Deleting platform-tools-latest-windows.zip and platform-tools_old"
+del "platform-tools-latest-windows.zip"
+@RD /S /Q "platform-tools_old"
+echo "Update done successfully"
